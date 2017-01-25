ea71126f878d8d3050e33f5593216f331a3e6341
Liang <liang6872@gmail.com>
2017-01-26 07:04:47 +0800

    fix mobile pay bug

--- protected/views/store/alipayapi.php
+++ protected/views/store/alipayapi.php
@@ -22,7 +22,7 @@
      */
 
 	require __DIR__ . '/../../../vendor/autoload.php';
-
+	require_once("/lib/alipay_submit.class.php");
 	require_once("alipay_config.php");
 
 
@@ -32,13 +32,14 @@
 	$out_trade_no = $_POST['id'];
 
 	//订单名称 必填
-	$subject = '我要下单！';
+	$subject = '店家!买单！';
 
 	//付款金额 必填
 	$total_fee = '';
 
 	//订单描述
-	$body = '好饿好饿！';
+	$body = '把好吃的全上一遍！';
+
 	//商品展示地址
 	$show_url = '';//$_POST['WIDshow_url'];
 	//需以http://开头的完整路径，例如：http://www.xxx.com/myorder.html
@@ -75,11 +76,9 @@
 		$euro_amount=isIsset(  normalPrettyPrice($data2['total']['total']) );
 		$exchange_rate= Yii::app()->functions->getOptionAdmin('rmb_amount');
 		$cny_amount = $euro_amount * $exchange_rate;
-		$total_fee=displayPrice(prettyFormat($cny_amount,$merchant_id));
+		$total_fee=prettyFormat($cny_amount,$merchant_id);
 
 	}
-
-
 
 	/************************************************************/
 
@@ -89,15 +88,30 @@
 	$alipay = new mytharcher\sdk\alipay\Alipay($alipay_config, $is_mobile);
 
 	if ($is_mobile) {
-		$params = $alipay->prepareMobileTradeData(array(
-			'out_trade_no' => $out_trade_no,
-			'subject'      => $subject,
-			'body'         => $body,
-			'total_fee'    => $total_fee,
-			'merchant_url' => 'http://'.$_SERVER['HTTP_HOST'],
-			'req_id'       => date('Ymdhis-')
-		));
-		echo $alipay->buildRequestFormHTML($params, 'get');
+
+//构造要请求的参数数组，无需改动
+		$parameter = array(
+			"service"       => $alipay_config['service'],
+			"partner"       => $alipay_config['partner'],
+			"seller_id"  => $alipay_config['seller_id'],
+			"payment_type"	=> $alipay_config['payment_type'],
+			"notify_url"	=> $alipay_config['notify_url'],
+			"return_url"	=> $alipay_config['return_url'],
+			"_input_charset"	=> trim(strtolower($alipay_config['input_charset'])),
+			"out_trade_no"	=> $out_trade_no,
+			"subject"	=> $subject,
+			"total_fee"	=> $total_fee,
+			"show_url"	=> $show_url, //$show_url
+			//"app_pay"	=> "Y",//启用此参数能唤起钱包APP支付宝
+			"body"	=> '', //$body
+			//其他业务参数根据在线开发文档，添加参数.文档地址:https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.2Z6TSk&treeId=60&articleId=103693&docType=1
+			//如"参数名"	=> "参数值"   注：上一个参数末尾需要“,”逗号。
+
+		);
+//建立请求
+		$alipaySubmit = new AlipaySubmit($alipay_config);
+		$html_text = $alipaySubmit->buildRequestForm($parameter,"get", "确认");
+		echo $html_text;
 	} else {
 		echo $alipay->buildRequestFormHTML(array(
 			"service"       => "create_direct_pay_by_user",
