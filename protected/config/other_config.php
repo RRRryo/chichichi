28005d4f278902815eccae84ffdd5d2bc81cc820
Liang <liang6872@gmail.com>
2017-02-08 16:18:25 +0100

    GOOGLE_MAP_URL

---
+++ protected/config/other_config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+//    'proxy' => 'proxy.priv.atos.fr:3128',   //bureau use
+    'google_map_url' => '".GOOGLE_MAP_URL."', //noral
+//    'google_map_url' => 'ditu.gdgdocs.org', //with proxy
+
+);
+
+?>
