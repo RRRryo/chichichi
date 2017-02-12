34a32d2aeb9595ed93e0235533a6086e9904b819
Liang <liang6872@gmail.com>
2017-02-13 01:39:46 +0800

    reset width

--- protected/views/front/address-book.php
+++ protected/views/front/address-book.php
@@ -127,9 +127,9 @@ $do=isset($_GET['do'])?$_GET['do']:'';
 <table id="table_list" class="table table-striped">
   <thead>
   <tr>
-   <th width="40%" ><?php echo Yii::t("default","Address")?></th>
-   <th ><?php echo Yii::t("default","Location Name")?></th>
-   <th ><?php echo Yii::t("default","Default")?></th>   
+   <th width="50%" ><?php echo Yii::t("default","Address")?></th>
+   <th width="40%"><?php echo Yii::t("default","Location Name")?></th>
+   <th width="10%"><?php echo Yii::t("default","Default")?></th>
   </tr>
   </thead>
 </table>  
