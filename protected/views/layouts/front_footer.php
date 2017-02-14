dd7388cc274063daba2cb4e2a75bfc682dd02b59
Liang <liang6872@gmail.com>
2017-02-15 03:25:55 +0800

    use scriptManager to manage js

--- protected/views/layouts/front_footer.php
+++ protected/views/layouts/front_footer.php
@@ -2,6 +2,6 @@
 /*add global variables to footer*/
 ScriptManager::registerGlobalVariables();
 ?>
-<script src="//www.google.com/recaptcha/api.js?onload=onloadMyCallback&render=explicit" async defer ></script>
+<!--<script src="//www.google.com/recaptcha/api.js?onload=onloadMyCallback&render=explicit" async defer ></script>-->
 </body>
 </html>
--- protected/views/layouts/front_top_menu.php
+++ protected/views/layouts/front_top_menu.php
@@ -53,7 +53,7 @@
     <?php $this->widget('zii.widgets.CMenu', FunctionsV3::getMenu('mobile-menu') );?>
     <div class="clear"></div>
 </div> <!--menu-top-menu-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
 <script>
 $(document).ready(function(){
 
