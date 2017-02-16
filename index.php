f749b0511139bc93c0be39a4ecfbe3b44aef19c9
Liang <liang6872@gmail.com>
2017-02-16 17:10:06 +0100

    date_default_timezone_set('Europe/Paris');

--- index.php
+++ index.php
@@ -37,9 +37,11 @@ define('YII_ENABLE_ERROR_HANDLER', false);
 define('YII_ENABLE_EXCEPTION_HANDLER', false);
 define('ROOTPATH', __DIR__);
 define('GOOGLE_MAP_URL', 'maps.googleapis.com');
+//define('GOOGLE_MAP_URL', 'ditu.gdgdocs.org');
 
-ini_set("display_errors",false);
+ini_set("display_errors",true);
 ini_set('error_log', ROOTPATH . "/php_error.log");
+date_default_timezone_set('Europe/Paris');
 
 //echo ini_get('error_log');
 //error_reporting(E_ERROR | E_WARNING | E_PARSE);
