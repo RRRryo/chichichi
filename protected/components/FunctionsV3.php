bfe01ffc661954fa89a4eedbd6006d13243e473f
Liang <liang6872@gmail.com>
2017-02-17 16:50:56 +0100

    optimize search list view getFreeDeliveryTagAlignCenter

--- protected/components/FunctionsV3.php
+++ protected/components/FunctionsV3.php
@@ -642,12 +642,28 @@ class FunctionsV3
 		$html = '';
     	if ($fee>0){
 			$html='<p><span class="label label-default">'. t("Free Domicile Delivery On Orders Over")." ". self::prettyPrice($fee).'</span></p>';
-    	}
-		if ($metro_fee>0){
-			$html.= '<p><span class="label label-default">'. t("Free Metro Delivery On Orders Over")." ". self::prettyPrice($metro_fee).'</span></p>';
+    	} else if ($metro_fee>0){
+			$html= '<p><span class="label label-default">'. t("Free Metro Delivery On Orders Over")." ". self::prettyPrice($metro_fee).'</span></p>';
+		} else {
+			$html.='<p>&nbsp;</p>';
 		}
     	return $html;
     }
+
+	public static function getFreeDeliveryTagAlignCenter($merchant_id='')
+	{
+		$fee=getOption($merchant_id,'free_delivery_above_price');
+		$metro_fee=getOption($merchant_id,'free_metro_delivery_above_price');
+		$html = '';
+		if ($fee>0){
+			$html='<p class="text-center"><span class="label label-default">'. t("Free Domicile Delivery On Orders Over")." ". self::prettyPrice($fee).'</span></p>';
+		} else if ($metro_fee>0){
+			$html= '<p><span class="label label-default">'. t("Free Metro Delivery On Orders Over")." ". self::prettyPrice($metro_fee).'</span></p>';
+		} else {
+			$html.='<p class="text-center">&nbsp;</p>';
+		}
+		return $html;
+	}
     
     public static function getDeliveryEstimation($merchant_id='')
     {
