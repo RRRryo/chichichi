38d87e237452ae884831ccd1af718a6fe1c3d132
Liang <liang6872@gmail.com>
2017-02-18 20:07:34 +0800

    optimize text no found show

--- protected/views/store/search-results-nodata.php
+++ protected/views/store/search-results-nodata.php
@@ -19,9 +19,9 @@ echo CHtml::hiddenField('current_page_url',isset($current_page_url)?$current_pag
 <div class="sections section-search-results">
   <div class="container center">
      <h3><?php echo Yii::t("default","Oops. We're having trouble finding that address.")?></h3>
-     <p><?php echo Yii::t("default","Please enter your address in one of the following formats and try again. Please do NOT enter your apartment or floor number here.")?></p>
-    <p class="bold">- <?php echo Yii::t("default","Street address, city, state")?></p>
-    <p class="bold">- <?php echo Yii::t("default","Street address, city")?></p>
-    <p class="bold">- <?php echo Yii::t("default","Street address, zip code")?></p>
+     <p class="text-center"><?php echo Yii::t("default","Please enter your address according to google map auto-complete.")?></p>
+<!--    <p class="bold">- <?php /*echo Yii::t("default","Street address, city, state")*/?></p>
+    <p class="bold">- <?php /*echo Yii::t("default","Street address, city")*/?></p>
+    <p class="bold">- <?php /*echo Yii::t("default","Street address, zip code")*/?></p>-->
   </div> <!--container--> 
 </div> <!--section-search-results-->   
