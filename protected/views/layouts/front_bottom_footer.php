4a969d5c1341a3d3e619088aa118aa22b7bf4420
Liang <liang6872@gmail.com>
2017-02-28 13:35:52 +0100

    optimize footer

--- protected/views/layouts/front_bottom_footer.php
+++ protected/views/layouts/front_bottom_footer.php
@@ -34,19 +34,45 @@
 <div class="sections section-footer">
   <div class="container">
       <div class="row">
-      <div class="col-md-6 col-xs-6">
-          <div class="pull-right">
-              <img src="<?php echo assetsURL()."/images/client_contact_qr.jpg"?>">
-              <div class="center">近味客服</div>
+          <div class="col-md-6 col-xs-6">
+              <div class="pull-right">
+                  <img src="<?php echo assetsURL()."/images/client_contact_qr.jpg"?>">
+                  <div class="center">近味客服</div>
+              </div>
+          </div>
+          <div class="col-md-6 col-xs-6">
+              <div class="pull-left">
+                  <img src="<?php echo assetsURL()."/images/commerce_contact_qr.jpg"?>">
+                  <div class="center">近味商服</div>
+              </div>
           </div>
       </div>
-      <div class="col-md-6 col-xs-6">
-          <div class="pull-left">
-              <img src="<?php echo assetsURL()."/images/commerce_contact_qr.jpg"?>">
-              <div class="center">近味商服</div>
+      <div class="row top10">
+          <div class="col-md-6 col-xs-12">
+              <div class="center">
+                深圳市南山区南山大道中兴之家E座616
+              </div>
+          </div>
+          <div class="col-md-2 col-xs-4">
+              <div class="center">
+              成为家厨
+              </div>
+          </div>
+          <div class="col-md-2 col-xs-4">
+              <div class="center">
+              关于我们
+              </div>
+          </div>
+          <div class="col-md-2 col-xs-4">
+              <div class="center">
+              加入我们
+              </div>
           </div>
       </div>
-      </div>
-      <p class="center"> <a href="http://www.miitbeian.gov.cn/">粤ICP备16107051号</a></p>
+
+
+      <p class="center">
+          <a href="http://www.miitbeian.gov.cn/">粤ICP备16107051号</a>
+      </p>
   </div> <!--container-->
 </div> <!--section-footer-->
