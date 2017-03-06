334aa6d14631db7e004d2387eefa696f37eb22dd
Liang <liang6872@gmail.com>
2017-03-06 16:10:12 +0100

    fix guest checkout bug

--- protected/views/front/guest-checkou-form.php
+++ protected/views/front/guest-checkou-form.php
@@ -20,7 +20,7 @@
 
 <div class="row top10">
 <div class="col-md-10">
-<?php echo CHtml::textField('street',isIsset($client_info['street']),array(
+<?php echo CHtml::textField('client_address',isset($client_info['client_address']),array(
    'class'=>'grey-fields full-width',
    'placeholder'=>Yii::t("default","Street"),
    'data-validation'=>"required"
@@ -30,36 +30,7 @@
 
 <div class="row top10">
 <div class="col-md-10">
-<?php echo CHtml::textField('city',isIsset($client_info['city']),array(
-   'class'=>'grey-fields full-width',
-   'placeholder'=>Yii::t("default","City"),
-   'data-validation'=>"required"
-  ))?> 
-</div> 
-</div>
-
-<div class="row top10">
-<div class="col-md-10">
-<?php echo CHtml::textField('state',isIsset($client_info['state']),array(
-   'class'=>'grey-fields full-width',
-   'placeholder'=>Yii::t("default","State"),
-   'data-validation'=>"required"
-  ))?> 
-</div> 
-</div>
-
-<div class="row top10">
-<div class="col-md-10">
-<?php echo CHtml::textField('zipcode',isIsset($client_info['zipcode']),array(
-   'class'=>'grey-fields full-width',
-   'placeholder'=>Yii::t("default","Zip code")
-  ))?> 
-</div> 
-</div>
-
-<div class="row top10">
-<div class="col-md-10">
-<?php echo CHtml::textField('location_name',isIsset($client_info['location_name']),array(
+<?php echo CHtml::textField('location_name',isset($client_info['location_name']),array(
    'class'=>'grey-fields full-width',
    'placeholder'=>Yii::t("default","Apartment suite, unit number, or company name"),   
   ))?>
@@ -68,8 +39,8 @@
 
 <div class="row top10">
 <div class="col-md-10">
-<?php echo CHtml::textField('contact_phone',isIsset($client_info['contact_phone']),array(
-   'class'=>'grey-fields mobile_inputs',
+<?php echo CHtml::textField('contact_phone',isset($client_info['contact_phone']),array(
+   'class'=>'grey-fields mobile_inputs full-width',
    'placeholder'=>Yii::t("default","Mobile Number"),
    'data-validation'=>"required"  
   ))?> 
@@ -104,4 +75,20 @@
    'placeholder'=>Yii::t("default","Password"),   
   ))?>
  </div> 
-</div>          
+</div>
+
+
+<script type="text/javascript">
+
+    jQuery(document).ready(function() {
+        var google_auto_address= $("#google_auto_address").val();
+        if ( google_auto_address =="yes") {
+        } else {
+            $("#client_address").geocomplete({
+                country: $("#admin_country_set").val()
+            });
+        }
+    });
+
+
+</script>
