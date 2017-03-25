3e9bb962c4a80c2905f2534acce2f7436fcb8142
Liang <liang6872@gmail.com>
2017-03-25 22:28:27 +0800

    use  navbar-static-top

--- protected/views/layouts/front_top_menu.php
+++ protected/views/layouts/front_top_menu.php
@@ -1,4 +1,4 @@
-<nav id="header" class="navbar navbar-fixed-top">
+<nav id="header" class="navbar navbar-static-top" style="margin-bottom:0;background:#FF5733">
 <div id="header-container" class="container navbar-container">
 <div class="top-menu-wrapper <?php echo "top-".$action;?>">
 
@@ -11,15 +11,15 @@
     </a>
     <?php endif;?>
   </div>
-  
+
   <div class="col-xs-1 menu-nav-mobile border relative">
      <a href="#"><i class="ion-android-menu"></i></a>
   </div> <!--menu-nav-mobile-->
-  
+
   <?php if ( Yii::app()->controller->action->id =="menu"):?>
-  <div class="col-xs-1 cart-mobile-handle border relative">     
+  <div class="col-xs-1 cart-mobile-handle border relative">
       <div class="badge cart_count top8"></div>
-     <a href="javascript:;">       
+     <a href="javascript:;">
        <i class="ion-ios-cart"></i>
      </a>
   </div> <!--cart-mobile-handle-->
@@ -32,16 +32,16 @@
   <?php
 //         Widgets::languageBar("store",true);
     ?>
-      
-    <?php $this->widget('zii.widgets.CMenu', FunctionsV3::getMenu() );?> 
-     
+
+    <?php $this->widget('zii.widgets.CMenu', FunctionsV3::getMenu() );?>
+
         </div>
     <div class="clear"></div>
   </div>
-  
- 
-  
-  
+
+
+
+
 </div> <!--container-->
 
 </div> <!--END top-menu-->
